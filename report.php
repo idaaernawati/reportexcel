0d507b266381ace8a6cb7c1ff8297ad1600fb565
Ida Ernawati <125109186+idaaernawati@users.noreply.github.com>
2023-05-21 14:10:52 +0700

    Add files via upload

---
+++ report.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World !');
+
+$writter = new Xlsx ($spreadsheet);
+$writter->save ('hello world.xlsx');
+?>
